b30f9915662295a188faf3aa911f15c48168e481
Aritro-Git <141334772+Aritro-Git@users.noreply.github.com>
2025-06-24 18:05:43 +0530

    Update CVE InsightX Owner.py

--- pages/CVE InsightX Owner.py
+++ pages/CVE InsightX Owner.py
@@ -25,6 +25,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->
 """, unsafe_allow_html=True)
 
+st.markdown("""
+<script>
+console.log("GTM script injected!");
+</script>
+""", unsafe_allow_html=True)
+
 # Inject GTM <noscript>
 st.markdown("""
 <!-- Google Tag Manager (noscript) -->
